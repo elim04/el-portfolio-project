b391532b7664caec1faa65d1ceb0d75d52708215
Emily Lim <emily.elim04@gmail.com>
2021-03-31 15:57:15 -0400

    create basic router for pages in app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,25 @@
+import Nav from "./Nav";
+import About from "./About";
+import Projects from "./Projects";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div>Emily Lim</div>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <div>Hi there, I'm Emily!</div>
+      </div>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/projects">
+          <Projects />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
