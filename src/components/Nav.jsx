13136f58e9d2a7775cbaaa65e7649a28365f912c
Emily Lim <emily.elim04@gmail.com>
2021-04-01 21:46:14 -0400

    divide nav bar into two divs and add experiments linke

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
+import "./Nav.scss";
 export default function Nav() {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/projects">Projects</Link>
+    <nav className="navbar">
+      <div className="navbar_home">
+        <Link to="/">Home</Link>
+      </div>
+      <div className="navbar_pages">
+        <Link to="/about">About</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/experiments">Experiments</Link>
+      </div>
     </nav>
   );
 }
