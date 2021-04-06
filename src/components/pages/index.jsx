133b55d7592165423a37cd6cf4dee99c38614fdb
Emily Lim <emily.elim04@gmail.com>
2021-04-05 22:16:43 -0400

    create pages componentto store home component aka nav bar

---
+++ src/components/pages/index.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import Navbar from "../navbar";
+import Sidebar from "../sidebar";
+
+const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
+    </>
+  );
+};
+
+export default Home;
