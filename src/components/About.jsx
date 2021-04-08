92c8d4302f30bab2cae3ad6dd4280b5c5c2541c2
Emily Lim <emily.elim04@gmail.com>
2021-04-07 21:09:01 -0400

    add card with test style

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,23 @@
 import "./About.scss";
+import { makeStyles } from "@material-ui/core/styles";
 import { SliderData } from "./slider/SliderData";
 import { Card, CardContent } from "@material-ui/core";
 import ImageSlider from "./slider/ImageSlider";
 
+const useStyles = makeStyles({
+  root: {
+    width: 700,
+    height: 600,
+    alignSelf: "center",
+  },
+});
+
 export default function About() {
+  const classes = useStyles();
   return (
     <div className="About">
       <ImageSlider slides={SliderData} />
-      <Card>
+      <Card className={classes.root}>
         <CardContent>Hi I am emily!</CardContent>
       </Card>
     </div>
