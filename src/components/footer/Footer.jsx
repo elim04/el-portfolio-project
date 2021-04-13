430dad3a575c4f1060cea40b77865c478fadfb71
Emily Lim <emily.elim04@gmail.com>
2021-04-13 19:03:59 -0400

    add rel=noreferrer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 import "./Footer.scss";
@@ -6,10 +5,14 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <footer className="footer-icons">
-      <a href="https://github.com/elim04" target="_blank">
+      <a href="https://github.com/elim04" target="_blank" rel="noreferrer">
         <FaGithub />
       </a>
-      <a href="https://www.linkedin.com/in/emilylim04/" target="_blank">
+      <a
+        href="https://www.linkedin.com/in/emilylim04/"
+        target="_blank"
+        rel="noreferrer"
+      >
         <FaLinkedin />
       </a>
     </footer>
