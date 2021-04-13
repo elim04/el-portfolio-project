cfffcbf928cbf95ab8ae375841cb84ae265531f0
Emily Lim <emily.elim04@gmail.com>
2021-04-13 19:03:21 -0400

    add project layout and components

---
+++ src/components/projects/ProjectItem.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProjectItem = () => {
+  return <div>I am a project item</div>;
+};
+
+export default ProjectItem;
---
+++ src/components/projects/ProjectList.jsx
@@ -0,0 +1,11 @@
+import ProjectItem from "./ProjectItem";
+import "./Projects.scss";
+
+export default function Projects() {
+  return (
+    <div>
+      <ProjectItem></ProjectItem>
+      <ProjectItem></ProjectItem>
+    </div>
+  );
+}
