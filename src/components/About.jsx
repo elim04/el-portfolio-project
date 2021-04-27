8cb8b7598c7a47db127bc1f91354311895b71f55
Emily Lim <emily.elim04@gmail.com>
2021-04-26 21:10:35 -0400

    add about text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import "./About.scss";
 import { makeStyles } from "@material-ui/core/styles";
 import { sliderData } from "./slider/SliderData";
-import { Card, CardContent } from "@material-ui/core";
+
 import ImageSlider from "./slider/ImageSlider";
 
 const useStyles = makeStyles({
@@ -15,9 +15,21 @@ const useStyles = makeStyles({
 export default function About() {
   const classes = useStyles();
   return (
-    <div className="About">
-      <div className="about-info"></div>
-      <p>Hi</p>
+    <div className="About-section">
+      <div className="about-text">
+        <h2 className="about-title">About Me</h2>
+        <p>
+          Hello! Let me introduce myself, my name is <strong>Emily</strong>. I
+          am a Food Scientist turned Software Developer based out of Toronto,
+          Ontario. I have always had a curious mind and been intrigued by
+          learning new skills or hobbies. That curiosity brought me to the world
+          of coding and ultimately a career pivot into tech. Coding allowed me
+          to challenge myself in new ways and feel fufilled in building
+          applications that people could use everyday. In my free time when I am
+          not coding, I enjoy travelling to new places, consuming ALL the new
+          foods and dabbling in some sewing projects.
+        </p>
+      </div>
       <ImageSlider slides={sliderData} />
     </div>
   );
