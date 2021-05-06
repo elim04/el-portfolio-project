b6fc8cf725e6c9c4313b5be3940b4e6a14c916bd
Emily Lim <emily.elim04@gmail.com>
2021-05-06 15:30:10 -0400

    revise global theme

--- src/theme/global.js
+++ src/theme/global.js
@@ -10,5 +10,7 @@ export const GlobalStyles = createGlobalStyle`
 body {
   background: ${({ theme }) => theme.body};
   color: ${({ theme }) => theme.text};
+  transition: all 0.25s linear;
 }
+
 `;
