10a2826e9c5be858d70a6898f39757e614ca1b2d
Emily Lim <emily.elim04@gmail.com>
2021-05-06 15:29:29 -0400

    change about paragraph

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,15 +10,16 @@ export default function About() {
       <div className="about-text">
         <h2 className="about-title">About Me</h2>
         <p>
-          Hello! Let me introduce myself, my name is <strong>Emily</strong>. I
-          am a Food Scientist turned Software Developer based out of Toronto,
-          Ontario. I have always had a curious mind and been intrigued by
-          learning new skills or hobbies. That curiosity brought me to the world
-          of coding and ultimately a career pivot into tech. Coding allowed me
-          to challenge myself in new ways and feel fufilled in building
-          applications that people could use everyday. In my free time when I am
-          not coding, I enjoy travelling to new places, consuming ALL the new
-          foods and dabbling in some sewing projects.
+          Hello! My name is <strong>Emily</strong> and I am a Food Scientist
+          turned Software Developer based out of Toronto, Ontario. I have always
+          had a curious mind and been intrigued by learning new skills or
+          hobbies. That curiosity is what brought me to the world of coding and
+          ultimately a career pivot into tech. Coding allowed me to challenge
+          myself in new ways and feel fufilled in building applications that
+          people could use everyday. In my free time when I am not coding, I
+          enjoy travelling to new places, taste testing ALL the new foods (don't
+          worry my love of food hasn't disappeared) and dabbling in some sewing
+          projects.
         </p>
       </div>
       <ImageSlider slides={sliderData} />
