b0559b79549e8e02a7325419e4471c438c2f66af
Emily Lim <emily.elim04@gmail.com>
2021-05-06 14:48:41 -0400

    add theme provider to app component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,28 +6,33 @@ import Footer from "./footer/Footer";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
 import Home from "./pages";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "../theme/theme";
+import { GlobalStyles } from "../theme/global";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Home />
-        <Switch>
-          <Route exact path="/">
-            <Landing />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/projects">
-            <Projects />
-          </Route>
-          <Route path="/experiments">
-            <Experiments />
-          </Route>
-        </Switch>
-        <Footer />
-      </div>
+      <ThemeProvider theme={lightTheme}>
+        <div className="App">
+          <Home />
+          <Switch>
+            <Route exact path="/">
+              <Landing />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/projects">
+              <Projects />
+            </Route>
+            <Route path="/experiments">
+              <Experiments />
+            </Route>
+          </Switch>
+          <Footer />
+        </div>
+      </ThemeProvider>
     </Router>
   );
 }
